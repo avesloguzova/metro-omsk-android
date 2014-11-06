e01e257941cb4bafd5745cedcb0589439c3435a9
avesloguzova <sashickk@gmail.com>
2014-11-06 18:39:51 +0300

    Fix Graph

--- app/src/main/java/ru/omsk/metro/paths_search/Graph.java
+++ app/src/main/java/ru/omsk/metro/paths_search/Graph.java
@@ -2,48 +2,53 @@ package ru.omsk.metro.paths_search;
 
 import android.util.SparseIntArray;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 import ru.omsk.metro.model.Line;
 import ru.omsk.metro.model.Station;
+import ru.omsk.metro.model.SubwayMap;
 import ru.omsk.metro.model.TimeInfinity;
 import ru.omsk.metro.model.WayStation;
 
 /**
  * Created by avesloguzova on 05.11.14.
- *
  */
 public class Graph {
+    private Vertex[] vertexes;
+    private SparseIntArray vertexesMap;// station id to index of vertex
 
-    public static Graph fromModel(List<Line>lines,List<WayStation> transitions){
-        int count=0;
-        for (Line line:lines){
-            count+=line.getStations().size();
+    public Graph(@NotNull SubwayMap subwayMap) {
+        int count = 0;
+        for (Line line : subwayMap.getLines()) {
+            count += line.getStations().size();
         }
-        Graph graph = new Graph(count);
+        vertexes = new Vertex[count];
+        vertexesMap = new SparseIntArray(count);
         int last_id = 0;
-        for(Line line:lines)
-            for (Station station:line.getStations()){
-               Vertex v = new Vertex(last_id);
-                graph.vertexes[last_id] = v;
-                if(!station.getTimeToNext().equals(TimeInfinity.getInstance()))
-                    v.edges.add(new Edge(last_id+1,station.getTimeToNextInSeconds()));
-                    //graph.weights[last_id][last_id+1] = station.getTimeToNextInSeconds();
-                if(!station.getTimeToPrevious().equals(TimeInfinity.getInstance()))
-                    v.edges.add(new Edge(last_id-1,station.getTimeToNextInSeconds()));
-                graph.vertexesMap.put(station.getId(), last_id);
+        for (Line line : subwayMap.getLines())
+            for (Station station : line.getStations()) {
+                Vertex v = new Vertex(station.getId());
+                vertexes[last_id] = v;
+                if (!station.getTimeToNext().equals(TimeInfinity.getInstance()))
+                    v.edges.add(new Edge(last_id + 1, station.getTimeToNextInSeconds()));
+                if (!station.getTimeToPrevious().equals(TimeInfinity.getInstance()))
+                    v.edges.add(new Edge(last_id - 1, station.getTimeToPreviousInSecondes()));
+                vertexesMap.put(station.getId(), last_id);
                 last_id++;
             }
-        for(WayStation transition:transitions){
-            int from = graph.vertexesMap.get(transition.getIdFrom());
-            int to = graph.vertexesMap.get(transition.getIdTo());
-            graph.vertexes[from].edges.add(new Edge(to, transition.getWayTimeInSeconds()));
+        for (WayStation transition : subwayMap.getWayStations()) {
+            int from = vertexesMap.get(transition.getIdFrom());
+            int to = vertexesMap.get(transition.getIdTo());
+            vertexes[from].edges.add(new Edge(to, transition.getWayTimeInSeconds()));
         }
-       return graph;
     }
-    public GraphPath getpath(int fromId,int toId){
+
+    @NotNull
+    public GraphPath getpath(int fromId, int toId) {
 
         int[] d = new int[vertexes.length];
         for (int i = 0; i < d.length; i++) {
@@ -53,19 +58,19 @@ public class Graph {
         boolean[] used = new boolean[vertexes.length];
         int start = vertexesMap.get(fromId);
         int end = vertexesMap.get(toId);
-        d[start]=0;
-        for (int i=0; i<vertexes.length; ++i) {
+        d[start] = 0;
+        for (int i = 0; i < vertexes.length; ++i) {
             int v = -1;
-            for (int j=0; j<vertexes.length; ++j)
+            for (int j = 0; j < vertexes.length; ++j)
                 if (!used[j] && (v == -1 || d[j] < d[v]))
                     v = j;
             if (d[v] == Integer.MAX_VALUE)
                 break;
             used[v] = true;
 
-            for (Edge e:vertexes[v].edges) {
+            for (Edge e : vertexes[v].edges) {
                 int to = e.idTo;
-                 int len = e.weight;
+                int len = e.weight;
                 if (d[v] + len < d[to]) {
                     d[to] = d[v] + len;
                     p[to] = v;
@@ -73,29 +78,24 @@ public class Graph {
             }
         }
         List<Integer> path = new LinkedList<Integer>();
-
-        for (int v=end; v!=start; v=p[v]){
-            path.add (v);
+        if (d[end] != Integer.MAX_VALUE) {
+            for (int v = end; v != start; v = p[v]) {
+                path.add(vertexes[v].external_id);
+            }
+            path.add(vertexes[start].external_id);
+            Collections.reverse(path);
         }
-        path.add(start);
-        Collections.reverse(path);
-        return new GraphPath(path,d[end]);
+        return new GraphPath(path, d[end]);
 
     }
-    private Graph(int count){
-        vertexes = new Vertex[count];
-        vertexesMap = new SparseIntArray(count);
-    }
-    private Vertex[] vertexes;
-    private SparseIntArray vertexesMap;
-
 
     private static class Vertex {
-        int id;
+        int external_id;
         List<Edge> edges;
 
-        public Vertex(int id) {
-            this.id=id;
+        public Vertex(int external_id) {
+            this.external_id = external_id;
+            this.edges = new LinkedList<Edge>();
         }
     }
 
